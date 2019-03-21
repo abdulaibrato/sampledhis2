8aaa9533cb846e89539ca1e601e6da49864458d4
Abdulai Brato Kamara <a.brato@ubuntuafrika.com>
2019-03-21 14:47:31 +0000

    Adding our sample dhis2 code

---
+++ src/main/java/com/example/sampledhis2/Sampledhis2Application.java
@@ -0,0 +1,13 @@
+package com.example.sampledhis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sampledhis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sampledhis2Application.class, args);
+    }
+
+}
